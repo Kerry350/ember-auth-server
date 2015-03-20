519e7f4b56ea22837ceb579608bf0933e2707871
Kerry Gallagher <k.gallagher.05@gmail.com>
2015-03-20 17:08:50 +0000

    Facebook

--- app/controllers/authentication/tokens.js
+++ app/controllers/authentication/tokens.js
@@ -2,7 +2,7 @@ module.exports = {
   // Takes an authorisation code and exchanges it for an access token
   // The access token is then used to acquire a profile, which can 
   // then be used to link the 3rd Party information to a user account
-  exchangeFacebookCodeForToken: function() {
-
+  authenticateWithFacebook: function(req, res) {
+    
   }
 };
--- app/routes/api/v1-api/tokens.js
+++ app/routes/api/v1-api/tokens.js
@@ -4,7 +4,6 @@ var oauth2orizeServer = require('./../../../authentication/oauth2orize').server;
 module.exports = {
   register: function(router) {
     router.post('/token', oauth2orizeServer.token());
-    router.post('/facebook/token', Controller.exchangeFacebookCodeForToken);
-    router.post('/github/token', Controller.exchangeGitHubCodeForToken)
+    router.post('/facebook/token', Controller.authenticateWithFacebook);
   }
 };
